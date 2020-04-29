ed1e2b3dc60ffff7eaf95e44c11925809d2f0039
Anekel <43749071+Anekel@users.noreply.github.com>
2020-04-29 13:45:54 +0200

    renamed mainwindow to GUI

--- GameGUI/GUI.xaml.cs
+++ GameGUI/GUI.xaml.cs
@@ -18,11 +18,23 @@ namespace GameGUI
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class GUI : Window
     {
-        public MainWindow()
+        public GUI()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (HelloButton.IsChecked == true)
+            {
+                MessageBox.Show("Hello.");
+            }
+            else if (GoodbyeButton.IsChecked == true)
+            {
+                MessageBox.Show("Goodbye");
+            }
         }
     }
 }
