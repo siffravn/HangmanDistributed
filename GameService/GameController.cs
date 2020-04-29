9feb15532f33d8063633597139c0b146e16a016f
Siff <siff.ravn@gmail.com>
2020-04-29 13:36:59 +0200

    WCF Prep service contract implementation

--- GameService/GameController.cs
+++ GameService/GameController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GameService.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
@@ -10,5 +11,31 @@ namespace GameService
     public class GameController : IGameController
     {
 
+        private static Dictionary<int, Hangman> games = new Dictionary<int, Hangman>();
+
+        public int ComputeScore(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateGame(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Hangman Guess(int id, string guess)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Hangman NewGame(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TerminateGame(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
