7f809a1b5c2b48e2b1f0fe55f62ebd2dbacf974e
Siff <siff.ravn@gmail.com>
2020-04-29 15:21:28 +0200

    Add code to host GameService
    https://docs.microsoft.com/en-us/dotnet/framework/wcf/how-to-host-and-run-a-basic-wcf-service#add-code-to-host-the-service

--- GameServiceHost/Program.cs
+++ GameServiceHost/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
 using System.Text;
 using System.Threading.Tasks;
+using GameService;
 
 namespace GameServiceHost
 {
@@ -10,6 +13,37 @@ namespace GameServiceHost
     {
         static void Main(string[] args)
         {
+            // Step 1: Create a URI to serve as the base address.
+            Uri baseAddress = new Uri("http://localhost:8733/Design_Time_Addresses/GameService/Service1/");
+
+            // Step 2: Create a ServiceHost instance.
+            ServiceHost selfHost = new ServiceHost(typeof(GameController), baseAddress);
+
+            try
+            {
+                // Step 3: Add a service endpoint.
+                selfHost.AddServiceEndpoint(typeof(IGameController), new WSHttpBinding(), "CalculatorService");
+
+                // Step 4: Enable metadata exchange.
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                selfHost.Description.Behaviors.Add(smb);
+
+                // Step 5: Start the service.
+                selfHost.Open();
+                Console.WriteLine("The service is ready.");
+
+                // Close the ServiceHost to stop the service.
+                Console.WriteLine("Press <Enter> to terminate the service.");
+                Console.WriteLine();
+                Console.ReadLine();
+                selfHost.Close();
+            }
+            catch (CommunicationException ce)
+            {
+                Console.WriteLine("An exception occurred: {0}", ce.Message);
+                selfHost.Abort();
+            }
         }
     }
 }
