a0ab7129a47552cd3c586ba11ae809b40a62e127
Siff <siff.ravn@gmail.com>
2020-05-06 13:31:15 +0200

    (Client) Implement simple client program

--- GameServiceClient/Program.cs
+++ GameServiceClient/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GameServiceClient.ServiceReference1;
 
 namespace GameServiceClient
 {
@@ -10,6 +11,17 @@ namespace GameServiceClient
     {
         static void Main(string[] args)
         {
+            //Step 1: Create an instance of the WCF proxy.
+            GameControllerClient client = new GameControllerClient();
+
+            // Step 2: Call the service operations.
+            Hangman game = client.CreateGame(123);
+            Console.WriteLine(game.HiddenWord);
+
+            // Step 3: Close the client to gracefully close the connection and clean up resources.
+            Console.WriteLine("\nPress <Enter> to terminate the client.");
+            Console.ReadLine();
+            client.Close();
         }
     }
 }
