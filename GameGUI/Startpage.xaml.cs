a5de2188796d2456a0196f9fb7eaa7f20be94ab4
magnusbrok <magnus.brok@db-data.dk>
2020-05-06 16:15:16 +0200

    work on startpage

---
+++ GameGUI/Startpage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GameGUI
+{
+    /// <summary>
+    /// Interaction logic for Startpage.xaml
+    /// </summary>
+    public partial class Startpage : Window
+    {
+        public Startpage()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_startGame_Click(object sender, RoutedEventArgs e)
+        {
+            GUI gui = new GUI();
+            gui.Show();
+            this.Close();
+        }
+    }
+}
