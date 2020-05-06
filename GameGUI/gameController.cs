2968c30e9ebdb1f3a7c901010ffb26a4306d2ea9
magnusbrok <magnus.brok@db-data.dk>
2020-05-06 14:05:30 +0200

    Create gameController.cs

---
+++ GameGUI/gameController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameGUI
+{
+    class gameController
+    {
+        private string ordet = "DetRigtigeOrd";
+        private float
+
+    }
+}
