383ffec503e24ec4fffb2231910344ea1c0c868c
Siff <siff.ravn@gmail.com>
2020-05-06 16:04:27 +0200

    (GUI) Create contructor for gameController
    Co-Authored-By: hellaachari <hellaachari@users.noreply.github.com>

--- GameGUI/gameController.cs
+++ GameGUI/gameController.cs
@@ -1,13 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ServiceReference1;
+using System.ServiceModel;
 
 namespace GameGUI
 {
     class gameController
     {
-        private string ordet = "DetRigtigeOrd";
-        private float
+        private int clientID;
+        private Hangman game;
+        GameControllerClient client;
+
+        public gameController()
+        {
+            BasicHttpBinding binding = new BasicHttpBinding();
+            EndpointAddress address = new EndpointAddress("http://localhost:8080/GameService/GameController");
+
+            client = new GameControllerClient(binding, address);
+        }
+
 
     }
 }
