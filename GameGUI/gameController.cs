4629737fc37f1d86ca8c9957f4da1277463361c0
Siff <siff.ravn@gmail.com>
2020-05-06 16:11:22 +0200

    Fix merge part II
    Co-Authored-By: hellaachari <hellaachari@users.noreply.github.com>
    Co-Authored-By: magnusbrok <magnusbrok@users.noreply.github.com>
    Co-Authored-By: anekel <anekel@users.noreply.github.com>

--- GameGUI/gameController.cs
+++ GameGUI/gameController.cs
@@ -7,38 +7,7 @@ using System.ServiceModel;
 
 namespace GameGUI
 {
-    public class GameController
-    {
-       
-        public string HiddenWord { get; set; }
-
-        
-        public string VisibleWord { get; set; }
-
-       
-        public int Lives { get; set; }
-
-      
-        public ArrayList UsedLetters { get; set; } = new ArrayList();
-
-        public bool IsGameWon { get; set; }
-
-       
-        public bool IsGameLost { get; set; }
-
-     
-        public bool IsGuessCorrect { get; set; }
-
-
-        public string Guess(string guessedWord)
-        {
-
-            Console.WriteLine(guessedWord);
-            //TODO call guessword in backend
-            VisibleWord = guessedWord;
-            Lives++;
-            
-            return guessedWord;
+    
     class gameController
     {
         private int clientID;
@@ -53,6 +22,15 @@ namespace GameGUI
             client = new GameControllerClient(binding, address);
         }
 
+        public string Guess(string guessedWord)
+        {
 
+            Console.WriteLine(guessedWord);
+            //TODO call guessword in backend
+            game.VisibleWord = guessedWord;
+            game.Lives++;
+
+            return guessedWord;
+        }
     }
 }
